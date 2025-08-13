0430664d05860def93749f6e1483ef294ec98eea
Hendrius Felix <hendriusfelix.dev@gmail.com>
2025-08-12 21:55:21 -0300

    feat: integrate Vuetify for UI components and directives in main application

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from '@/router'
-import vuetify from '@/plugins/vuetify'
 import App from '@/App.vue'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
