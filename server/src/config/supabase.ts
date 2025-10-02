e74ccf37797036ec9b7dc171f0995f39088c253a
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-10-02 13:30:13 -0300

    refactor: remove todas as configurações que não eram do client (api e software) deixando apenas configurações do app

--- server/src/config/supabase.ts
+++
@@ -1,4 +0,0 @@
-import { config } from "../config/dotenv";
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(config.supabaseUrl, config.supabaseServiceKey);
